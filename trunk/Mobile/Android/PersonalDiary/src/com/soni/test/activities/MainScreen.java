2a97262cd50fd7cbaf07973d826abd195d647312
sandeep.soni <sandeep.soni@32f56017-7c33-0410-beea-25b907642fa6>
2009-09-24 16:36:24 +0000

    First Version of the Personal Diary Application

---
+++ trunk/Mobile/Android/PersonalDiary/src/com/soni/test/activities/MainScreen.java
@@ -0,0 +1,53 @@
+/*
+Copyright 2004 The Apache Software Foundation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Developed by : Sandeep Soni [ http://sonisandeep.blogspot.com, 
+    			      Email : Sandeep.Soni at yahoo.com 
+    			    ]
+*/
+package com.soni.test.activities;
+
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+import com.soni.test.GridViewDayAdapter;
+import com.soni.test.PersonalDiaryDB;
+import com.soni.test.R;
+
+/**
+ * @author Sandeep Soni
+*/
+
+public class MainScreen extends Activity {
+
+    private PersonalDiaryDB _db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	_db = new PersonalDiaryDB(this);
+	
+	setContentView(R.layout.main);
+	setUpCalendar();
+    }
+
+    private void setUpCalendar() {
+	GridView calView = (GridView) findViewById(R.id.calGridView);
+	calView.setAdapter(new GridViewDayAdapter(this,Calendar.getInstance().getTime(),_db));
+    }
+}
